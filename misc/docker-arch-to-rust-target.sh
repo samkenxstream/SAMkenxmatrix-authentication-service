cab94fd3e9d171c563656331d6f004f3b375d4ed
Quentin Gliech <quenting@element.io>
2021-12-09 23:26:32 +0100

    Use Buildx bake files for better control over the docker images

--- misc/docker-arch-to-rust-target.sh
+++ misc/docker-arch-to-rust-target.sh
@@ -7,8 +7,14 @@ fi
 
 if [ "$1" = "linux/arm64" ]; then
   echo "aarch64-unknown-linux-gnu"
+elif [ "$1" = "linux/arm/v6" ]; then
+  echo "x86_64-unknown-linux-gnu"
+elif [ "$1" = "linux/arm/v7" ]; then
+  echo "x86_64-unknown-linux-gnu"
 elif [ "$1" = "linux/amd64" ]; then
   echo "x86_64-unknown-linux-gnu"
+elif [ "$1" = "linux/386" ]; then
+  echo "i686-unknown-linux-gnu"
 else
   echo "unsupported platform $1" >&2
   exit 2
